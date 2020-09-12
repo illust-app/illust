e9706dffe2724c80fa5f459796dd7c20ce5a83c4
home_machine <illust.training@gmail.com>
2020-09-13 03:58:16 +0900

    add batch_size and epochs

--- train.py
+++ train.py
@@ -9,6 +9,10 @@ from model.SRCNN import SRCNN
 from trainer import Trainer
 
 
+batch_size = 5
+epochs = 5
+
+
 device = 'cpu'
 
 
@@ -20,13 +24,13 @@ test_data_path = os.path.join(data_path, 'test_patch')
 if __name__ == '__main__':
 
     train_dataset = SRDataset(train_data_path)
-    train_dataloader = torch.utils.data.DataLoader(train_dataset, batch_size=5, shuffle=True)
+    train_dataloader = torch.utils.data.DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
     test_dataset = SRDataset(test_data_path)
-    test_dataloader = torch.utils.data.DataLoader(test_dataset, batch_size=5, shuffle=True)
+    test_dataloader = torch.utils.data.DataLoader(test_dataset, batch_size=batch_size, shuffle=True)
     model = SRCNN(3, 3, features=[32, 64, 128, 256, 128, 64, 32], activation='frelu').to(device)
-    # summary(model, (3, 64, 64))
+    summary(model, (3, 64, 64))
     criterion = torch.nn.MSELoss().to(device)
     params = list(model.parameters())
     optim = torch.optim.Adam(params=params, lr=1e-3)
-    trainer = Trainer(model, criterion, optim, shape=(5, 3, 64, 64))
-    trainer.train(5, train_dataloader, test_dataloader)
+    trainer = Trainer(model, criterion, optim, shape=(5, 3, 64, 64), colab_flas=True)
+    trainer.train(epochs, train_dataloader, test_dataloader)
