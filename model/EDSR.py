a6eb70858ace0490c11c71450c1c3250b4c2d7e9
home_machine <illust.training@gmail.com>
2020-09-14 03:48:33 +0900

    half to block faeture

--- model/EDSR.py
+++ model/EDSR.py
@@ -13,14 +13,9 @@ class EDSR(torch.nn.Module):
         activation = kwargs.get('activation')
         self.scale = scale
         self.start_conv = torch.nn.Conv2d(input_ch, feature, 3, 1, 1)
-        self.edsr_block = torch.nn.ModuleList([EDSR_Block(input_ch=feature, output_ch=feature, feature=feature * 2, activation=activation) for _ in range(block_num)])
+        self.edsr_block = torch.nn.ModuleList([EDSR_Block(input_ch=feature, output_ch=feature, feature=feature, activation=activation) for _ in range(block_num)])
         self.res_conv = torch.nn.Conv2d(feature, output_ch, 3, 1, 1)
         self.factor_conv = torch.nn.Conv2d(output_ch, feature, 1, 1)
-        # if scale == 3:
-        #     self.up_factor = torch.nn.Sequential(torch.nn.Conv2d(feature, feature * 3 ** 2, 3, 1, 1), torch.nn.PixelShuffle(3))
-        # else:
-        # self.up_factor = torch.nn.ModuleList([[*torch.nn.Sequential(torch.nn.Conv2d(feature, feature * scale ** 2, 3, 1, 1), torch.nn.PixelShuffle(int(np.log2(scale))))] for _ in range(int(np.log2(scale)))])
-        # self.up_factor = torch.nn.ModuleList([Conv2d_Shuffle(feature, scale) for _ in range(int(np.log2(scale)))])
         self.up_factor = Conv2d_Shuffle(feature, scale)
         self.output_conv = torch.nn.Conv2d(feature, output_ch, 3, 1, 1)
 
