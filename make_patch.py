75c4646dc0690c88ed461a32ab003afb7e5fb52b
home_machine <illust.training@gmail.com>
2020-09-13 15:25:25 +0900

    add eval move dataset

--- make_patch.py
+++ make_patch.py
@@ -16,9 +16,9 @@ if __name__ == '__main__':
     draw_path = os.path.join(save_path, 'draw')
     draw_patch_path = os.path.join(save_path, 'draw_patch')
     np.random.seed(123456)
-    data_list = os.listdir(data_path)[:5]
+    data_list = os.listdir(data_path)[:6]
     data_list.sort()
-    train_test_idx = np.random.choice((0, 1), len(data_list), p=(.8, .2))
+    train_test_idx = np.random.choice((0, 1), len(data_list), p=(.5, .5))
     data_list = np.array(data_list)
     train_list = list(data_list[train_test_idx == 0])
     test_list = list(data_list[train_test_idx == 1])
@@ -26,5 +26,5 @@ if __name__ == '__main__':
     make_patch(train_path, train_patch_path, size=64, ch=3)
     move_data(data_path, test_list, test_path)
     make_patch(test_path, test_patch_path, size=64, ch=3)
-    move_data(data_path, test_list[:max(1, int(len(test_list) * .2))], draw_path)
+    move_data(data_path, test_list[:max(2, int(len(test_list) * .2))], draw_path)
     # make_patch(draw_path, draw_patch_path, size=256, ch=3)
