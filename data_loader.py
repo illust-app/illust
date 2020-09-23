57c71750dc8bf98dd423e063f5b23afd99f8c094
home_machine <illust.training@gmail.com>
2020-09-24 01:17:23 +0900

    use unsqueeze

--- data_loader.py
+++ data_loader.py
@@ -69,6 +69,7 @@ class SRCNNDataset(SRDataset):
         input_data = img.resize((h // self.scale, w // self.scale))
         input_data = img.resize((h * self.scale, w * self.scale))
         '''
-        input_data = torch.nn.functional.adaptive_avg_pool2d(input_data, (h // self.scale, w // self.scale))
+        input_data = nd_data.unsqueeze(0)
+        input_data = torch.nn.functional.adaptive_avg_pool2d(input_data, (h // self.scale, w // self.scale)).squeeze()
         input_data = torch.nn.functional.interpolate(input_data, (h * self.scale, w * self.scale), mode='bicubic', align_corners=False)
         return input_data, label_data
